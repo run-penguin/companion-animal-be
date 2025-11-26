a70e81b7c0ab87ab64f73708ea8fcdd3dd80c42c
runpenguin <run-penguin@naver.com>
2025-11-26 17:41:14 +0900

    vercel cors 설정에 추가

--- src/main/java/com/companion_animal/config/CorsConfig.java
+++ src/main/java/com/companion_animal/config/CorsConfig.java
@@ -15,7 +15,9 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 //                WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://companion-animal-nw89psks8-runpenguins-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
